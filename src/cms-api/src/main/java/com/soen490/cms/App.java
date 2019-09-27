0bb3a1e73ed273dc95a0c2d7f595e1acc5f08fd1
Scott Bouchard <scottbouchard09@gmail.com>
2019-09-26 22:18:49 -0400

    add logging library (log4j2) and store logs in a file #7

--- src/cms-api/src/main/java/com/soen490/cms/App.java
+++ src/cms-api/src/main/java/com/soen490/cms/App.java
@@ -2,11 +2,14 @@ package com.soen490.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
+@Log4j2
 public class App {
 
 	public static void main(String[] args) {
+		log.info("Running CMS app");
 		SpringApplication.run(App.class, args);
 	}
 
--- src/cms-api/src/test/java/com/soen490/cms/AppTests.java
+++ src/cms-api/src/test/java/com/soen490/cms/AppTests.java
@@ -4,13 +4,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppTests {
 
+	private static Logger log = LogManager.getLogger(AppTests.class);
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void logTest() {
+		log.info("This is a log test");
 	}
 
 }
