a18da6745b4cb71ee191199381dfedeba7ab5067
MBenchimol <32246260+MBenchimol@users.noreply.github.com>
2019-09-28 16:48:44 -0400

    #5 Add backbone for backend calls and add Angular Material to project

--- src/cms-client/src/app/app.component.ts
+++ src/cms-client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './backend-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'cms-client';
+  featureFlagTest = 'test';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getFeatureFlagTest().subscribe((val: string) => {
+        console.log(val);
+        this.featureFlagTest = val;
+      });
+  }
+
 }
--- src/cms-client/src/app/app.module.ts
+++ src/cms-client/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
