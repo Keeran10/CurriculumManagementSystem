416c16603d1c8485b98135295268d548379f0f23
MBenchimol <32246260+MBenchimol@users.noreply.github.com>
2019-09-28 16:29:19 -0400

    #5 Add feature flags to backend with test feature

--- src/cms-api/src/main/java/com/soen490/cms/App.java
+++ src/cms-api/src/main/java/com/soen490/cms/App.java
@@ -1,10 +1,30 @@
 package com.soen490.cms;
 
+import com.soen490.cms.FeatureFlagTest.FeatureFlagOff;
+import com.soen490.cms.FeatureFlagTest.FeatureFlagOn;
+import com.soen490.cms.FeatureFlagTest.IFeatureFlagTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class App {
+
+	@Bean
+	@Primary
+	@ConditionalOnProperty(prefix = "Feature.toggle", name = "featureFlagTest", havingValue="false")
+	public IFeatureFlagTest getFlagOff(){
+		return new FeatureFlagOff();
+	}
+
+	@Bean
+	@Primary
+	@ConditionalOnProperty(prefix = "Feature.toggle", name = "featureFlagTest", havingValue="true")
+	public IFeatureFlagTest getFlagOn(){
+		return new FeatureFlagOn();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
---
+++ src/cms-api/src/main/java/com/soen490/cms/FeatureFlagTest/IFeatureFlagTest.java
@@ -0,0 +1,5 @@
+package com.soen490.cms.FeatureFlagTest;
+
+public interface IFeatureFlagTest {
+    String getData();
+}
