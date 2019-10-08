6a5d39f8b66f9f00caf45a31898ea1b5dc146e84
Keeran10 <keeran.r10@gmail.com>
2019-10-07 22:23:41 -0400

    [#15] Add repositories with default implicit crud functions

---
+++ src/cms-api/src/main/java/com/soen490/cms/Repositories/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.soen490.cms.Repositories;
+
+import com.soen490.cms.Models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Integer>{
+}
