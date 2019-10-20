68b512fae50fa3b5d4c02ef35c931fcfb2f14edc
georgikbarsemian <georgik.barsemian@mail.concordia.ca>
2019-10-20 19:17:00 -0400

    [#39] Unit test for Request update, create and remove report

--- src/cms-api/src/test/java/com/soen490/cms/ImpactStatementUnitTest.java
+++ src/cms-api/src/test/java/com/soen490/cms/ImpactStatementUnitTest.java
@@ -5,6 +5,7 @@ import com.soen490.cms.Models.Request;
 import com.soen490.cms.Models.Requisite;
 import com.soen490.cms.Services.CourseService;
 import com.soen490.cms.Services.ImpactAssessmentCourseService;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -17,23 +18,80 @@ import static org.mockito.Mockito.when;
 public class ImpactStatementUnitTest {
 
     CourseService courseService = mock(CourseService.class);
+    Course course;
+    ImpactAssessmentCourseService impactAssessmentCourse;
 
-    @Test
-    public void ImpactStatementCourseCreation(){
-        ImpactAssessmentCourseService impactAssessmentCourse = new ImpactAssessmentCourseService();
-        impactAssessmentCourse.setServiceMock(courseService);
-        Request request = new Request();
-        request.setRequestType(1);
-        request.setTargetId(1);
-        Course course = new Course();
+    @Before
+    public void intiation(){
+        course = new Course();
         course.setId(5);
         course.setName("COMP");
         course.setNumber(362);
+        course.setCredits(5);
+        course.setDescription("test");
+        course.setLabHours(5);
+        course.setTutorialHours(3);
+        course.setLectureHours(1);
+        course.setLevel(2);
+        course.setTitle("Math");
         course.setRequisites(new ArrayList<>());
         when(courseService.findCourseById(1)).thenReturn(course);
+        impactAssessmentCourse = new ImpactAssessmentCourseService();
+        impactAssessmentCourse.setServiceMock(courseService);
+    }
+
+    @Test
+    public void ImpactStatementCourseCreation(){
+        Request request = new Request();
+        request.setRequestType(1);
+        request.setTargetId(1);
+
         when(courseService.findAllOccurrencesOfCourseAsRequisite(5)).thenReturn(new ArrayList<Requisite>());
         Map<String,Object> mapResponse = impactAssessmentCourse.getCourseImpact(request);
-        System.out.println(mapResponse);
-        assertThat(true).isEqualTo(true);
+
+        assertThat(mapResponse.containsValue(course.getName())).isEqualTo(true);
+        assertThat(mapResponse.containsValue(course.getNumber())).isEqualTo(true);
+    }
+
+    @Test
+    public void ImpactStatementCourseUpdate(){
+        Request request = new Request();
+        request.setRequestType(2);
+        request.setTargetId(2);
+        request.setOriginalId(1);
+
+        Course courseUpdated = new Course();
+        courseUpdated.setId(6);
+        courseUpdated.setName("COMP");
+        courseUpdated.setNumber(363);
+        courseUpdated.setTitle("Math");
+        courseUpdated.setCredits(5);
+        courseUpdated.setDescription("test");
+        courseUpdated.setLabHours(5);
+        courseUpdated.setTutorialHours(3);
+        courseUpdated.setLectureHours(1);
+        courseUpdated.setLevel(2);
+        courseUpdated.setRequisites(new ArrayList<>());
+
+        when(courseService.findCourseById(2)).thenReturn(courseUpdated);
+        when(courseService.findAllOccurrencesOfCourseAsRequisite(5)).thenReturn(new ArrayList<Requisite>());
+        when(courseService.findAllOccurrencesOfCourseAsRequisite(6)).thenReturn(new ArrayList<Requisite>());
+
+        Map<String,Object> mapResponse = impactAssessmentCourse.getCourseImpact(request);
+        assertThat(mapResponse.get("CourseChanges").toString()).isEqualTo("{Number="+courseUpdated.getNumber()+"}");
+    }
+
+    @Test
+    public void ImpactStatementCourseRemoval(){
+        Request request = new Request();
+        request.setRequestType(3);
+        request.setTargetId(1);
+
+
+        when(courseService.findAllOccurrencesOfCourseAsRequisite(5)).thenReturn(new ArrayList<Requisite>());
+        Map<String,Object> mapResponse = impactAssessmentCourse.getCourseImpact(request);
+
+        assertThat(mapResponse.containsValue(course.getName())).isEqualTo(true);
+        assertThat(mapResponse.containsValue(course.getNumber())).isEqualTo(true);
     }
 }
