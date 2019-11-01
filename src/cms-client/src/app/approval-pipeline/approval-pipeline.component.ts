9fac8fa0407ccb69b848bf3f9a2a02bffe617b9e
irinafakotakis <irinafakotakis@gmail.com>
2019-11-01 00:51:46 -0400

    #56 select approval pipeline UI

--- src/cms-client/src/app/approval-pipeline/approval-pipeline.component.ts
+++ src/cms-client/src/app/approval-pipeline/approval-pipeline.component.ts
@@ -10,16 +10,31 @@ export class ApprovalPipelineComponent {
 
     // if user selects to create a custom pipeline, store here
     public customPipeline = [];
+    // predefined pipeline order
+    public predefinedPipeline = ['Department Curriculum Committee',
+                                    'Department Council',
+                                    'Associate Dean Academic Programs Under Graduate Studies Committee',
+                                    'Faculty Council',
+                                    'APC',
+                                    'Senate'];
     public custom(opt: string[]) {
         let i;
         if (opt.length === 0) {
             alert('Cannot submit blank pipeline');
         } else {
-        for (i of opt) {
-            this.customPipeline.push(i.value);
-            console.log(i);
+            for (i of opt) {
+                this.customPipeline.push(i.value);
+                console.log(i.value);
+            }
         }
     }
+    // placeholder logs user selected predefined pipeline until next page is created that will need this information
+    public predefined() {
+        console.log('User selected predefined pipeline');
+        let i;
+        for (i of this.predefinedPipeline) {
+            console.log(i);
+        }
     }
 
 }
