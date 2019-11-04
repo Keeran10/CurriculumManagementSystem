e7e6581c12b6283959f63bf740de3869702ebf4b
Keeran10 <keeran.r10@gmail.com>
2019-11-04 14:53:59 -0500

    [#71] Fix SQL queries for degrees

--- src/cms-api/src/main/java/com/soen490/cms/Repositories/DegreeRepository.java
+++ src/cms-api/src/main/java/com/soen490/cms/Repositories/DegreeRepository.java
@@ -12,10 +12,10 @@ import java.util.Collection;
 public interface DegreeRepository extends JpaRepository<Degree, Integer>{
     Degree findByName(String name);
 
-    @Query(value = "SELECT * FROM degree d INNER JOIN degree_requirement ON d.id = degree_requirement.degree_id WHERE degree_requirement.course_id=?1 AND core != \"Elective\"", nativeQuery = true)
+    @Query(value = "SELECT * FROM degree d INNER JOIN degree_requirement ON d.id = degree_requirement.degree_id WHERE degree_requirement.course_id=?1 AND core LIKE '% Core'", nativeQuery = true)
     Collection<Degree> findDegreeByRequiredCourseId(int course_id);
 
-    @Query(value = "SELECT * FROM degree d INNER JOIN degree_requirement ON d.id = degree_requirement.degree_id WHERE degree_requirement.course_id=?1 AND core = \"Elective\"", nativeQuery = true)
+    @Query(value = "SELECT * FROM degree d INNER JOIN degree_requirement ON d.id = degree_requirement.degree_id WHERE degree_requirement.course_id=?1 AND core NOT LIKE '% Electives'", nativeQuery = true)
     Collection<Degree> findDegreeByElectiveCourseId(int course_id);
 
     @Query(value = "SELECT SUM(c.credits) FROM course c INNER JOIN degree_requirement ON c.id = degree_requirement.course_id WHERE degree_requirement.core=?1 AND c.is_active=1 ", nativeQuery = true)
