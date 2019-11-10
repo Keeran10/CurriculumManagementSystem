337b5436bbeaf44adcc9f6f91b17e2942b05cf9a
Antoine B <antoine.beiten@gmail.com>
2019-11-10 15:01:55 -0500

    #88 Log login inputs to console

--- src/cms-client/src/app/login/login.component.ts
+++ src/cms-client/src/app/login/login.component.ts
@@ -42,7 +42,7 @@ export class LoginComponent implements OnInit {
 
   OnSubmit(username, password) {
     this.api.setCredentials(username, password).subscribe((data: any) => {
-      localStorage.setItem('userToken', data.access_token);
+      localStorage.setItem('userToken', data.access_token), console.log(data);
     });
   }
 }
