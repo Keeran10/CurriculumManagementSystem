7676ea2c380461952165293848484c6e7ad84e4f
irinafakotakis <irinafakotakis@gmail.com>
2020-02-10 23:01:52 -0500

    #233 fix test

--- src/cms-client/src/app/calendar-sections/calendar-sections.component.spec.ts
+++ src/cms-client/src/app/calendar-sections/calendar-sections.component.spec.ts
@@ -1,17 +1,13 @@
 /* // MIT License
-
 // Copyright (c) 2019 teamCMS
-
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,6 +20,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ApiService } from '../backend-api.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { CookieService } from 'ngx-cookie-service';
 
 import { CalendarSectionsComponent } from './calendar-sections.component';
 
@@ -36,7 +33,8 @@ describe('CalendarSectionsComponent', () => {
       ],
       declarations: [CalendarSectionsComponent],
       providers: [
-        ApiService
+        ApiService,
+        CookieService
       ],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
